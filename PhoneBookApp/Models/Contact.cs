d02b18a3d8b02be8588a3959af11cb09617eec55
samek <samek@LAPTOP-S1NHS1NJ>
2023-07-17 17:46:12 -0700

    Add project files.

---
+++ PhoneBookApp/Models/Contact.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBookApp.Models
+{
+    internal class Contact
+    {
+        public int Id { get; set; }
+        // Set Name and PhoneNumber to null with null forgiving operator as we do not want these fields to be empty
+        public string Name { get; set; } = null!;
+        public string PhoneNumber { get; set; } = null;
+    }
+}
