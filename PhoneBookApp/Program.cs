5a13ed7f6972d046f60a8f72383bba1b1865a739
joshhkim02 <joshhkim02@gmail.com>
2023-07-31 16:24:39 -0700

    Removed unused code

--- PhoneBookApp/Program.cs
+++ PhoneBookApp/Program.cs
@@ -5,16 +5,4 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 Menu menu = new();
-Helpers helpers = new();
-UserInput input = new();
-Validate validate = new();
-using PhoneBookContext _context = new();
-
-HostApplicationBuilder builder = Host.CreateApplicationBuilder();
-
-builder.Services.AddSingleton<Program>();
-
-using IHost host = builder.Build();
-host.Run();
-
 menu.ShowMenu();
